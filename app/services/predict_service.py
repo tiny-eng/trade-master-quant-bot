7608c7c46aeb9fbf99550a69710cd6ff43fe6df1
tiny <tineneuron@outlook.com>
2025-11-27 18:10:55 -0500

    model

--- app/services/predict_service.py
+++ app/services/predict_service.py
@@ -7,44 +7,41 @@ from app.services.polygon_service import get_last_n_minutes
 from app.config.constants import DEVICE, PRED_INDEX, INPUT_LEN
 
 
-async def predict_live_price(symbol: str, target: int):
-    if target not in PRED_INDEX:
-        raise ValueError(f"Target must be one of: {PRED_INDEX}")
+async def predict_live_price(symbol: str):
+    # if target not in PRED_INDEX:
+    #     raise ValueError(f"Target must be one of: {PRED_INDEX}")
 
-    # 1) Load model
     model = load_model(symbol)
 
-    # 2) Get last price data
     prices = get_last_n_minutes(symbol, INPUT_LEN)
     prices = np.array(prices).reshape(-1, 1)
 
-    # 3) Fit scaler dynamically on last prices
     scaler = MinMaxScaler()
-    scaled = scaler.fit_transform(prices)  # ✅ Fit before transform
+    scaled = scaler.fit_transform(prices)  # 
 
-    # 4) Build input sequence
     X = create_input_sequence(scaled)
     X_tensor = torch.tensor(X, dtype=torch.float32).to(DEVICE)
 
-    # 5) Predict
     with torch.no_grad():
         y_pred = model(X_tensor).cpu().numpy()[0]
 
-    # 6) Extract specific prediction
-    idx = PRED_INDEX.index(target)
+    # idx = PRED_INDEX.index(target)
     # print(y_pred)
-    # for i in range (5):
-    #     predicted_scaled = y_pred[i].reshape(-1, 1)
-    #     predicted_price = scaler.inverse_transform(predicted_scaled)[0][0]
-    #     print(predicted_price)
+    predicted_price = []
+    for i in range (5):
+        predicted_scaled = y_pred[i].reshape(-1, 1)
+        price = scaler.inverse_transform(predicted_scaled)[0][0]
+        predicted_price.append(float(price))
+    
+    print(predicted_price)
 
-    predicted_scaled = y_pred[idx].reshape(-1, 1)
+    # predicted_scaled = y_pred[idx].reshape(-1, 1)
 
     # 7) Inverse transform to original scale
-    predicted_price = scaler.inverse_transform(predicted_scaled)[0][0]
+    # predicted_price = scaler.inverse_transform(predicted_scaled)[0][0]
 
     return {
         "symbol": symbol,
-        "target_min": target,
-        "prediction": round(float(predicted_price), 2)
+        # "target_min": target,
+        "prediction": predicted_price
     }
